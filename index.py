64298387945f19719d5f13d6746aa7e231917005
YuZhongChen2023 <a126055626@gmail.com>
2023-12-01 23:46:09 +0800

    make it better

--- index.py
+++ index.py
@@ -152,21 +152,17 @@ def movie():
 
 @app.route("/searchmovie", methods=["GET","POST"])
 def searchmovie():
-    if request.method == "POST":
-        MovieTitle = request.form["MovieTitle"]
-        info = ""
-        db = firestore.client()     
-        collection_ref = db.collection("電影")
-        docs = collection_ref.order_by("showDate").get()
-        for doc in docs:
-            if MovieTitle in doc.to_dict()["title"]: 
-                info += "片名：" + doc.to_dict()["title"] + "<br>" 
-                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
-                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
-                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
-        return info
-    else:  
-        return render_template("input.html")
+    info = ""
+    db = firestore.client()  
+    docs = db.collection("電影").get() 
+    for doc in docs:
+        if "飛鴨" in doc.to_dict()["title"]:
+            info += "片名：" + doc.to_dict()["title"] + "<br>" 
+            info += "海報：" + doc.to_dict()["picture"] + "<br>"
+            info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+            info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
+            info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
+    return info
 
 if __name__ == "__main__":
     app.debug = True
